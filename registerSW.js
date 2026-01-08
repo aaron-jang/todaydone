b643327fe7a906a2c56d459c48941292d106a94a
Aaron Jang <aaronjang.k@gmail.com>
2026-01-08 23:33:59 +0800

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/todaydone/sw.js', { scope: '/todaydone/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/todaydone/sw-custom.js', { scope: '/todaydone/' })})}
